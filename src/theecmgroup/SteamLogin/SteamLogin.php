db33d0cdd1d427005e25fa28c913bf00846009b9
Daedalus <daedalus@the-ecm-group.com>
2015-05-10 16:16:32 -0700

    Fix request params.

--- src/theecmgroup/SteamLogin/SteamLogin.php
+++ src/theecmgroup/SteamLogin/SteamLogin.php
@@ -81,16 +81,16 @@ class SteamLogin implements SteamLoginInterface {
 
         try {
             $params = array(
-                'openid.assoc_handle' => $_GET['openid_assoc_handle'],
-                'openid.signed' => $_GET['openid_signed'],
-                'openid.sig' => $_GET['openid_sig'],
+                'openid.assoc_handle' => $this->request->input('openid_assoc_handle'),
+                'openid.signed' => $this->request->input('openid_signed'),
+                'openid.sig' => $this->request->input('openid_sig'),
                 'openid.ns' => 'http://specs.openid.net/auth/2.0',
             );
 
-            $signed = explode(',', $_GET['openid_signed']);
+            $signed = explode(',', $this->request->input('openid_signed'));
 
             foreach ($signed as $item) {
-                $val = $_GET['openid_' . str_replace('.', '_', $item)];
+                $val = $this->request->input('openid_' . str_replace('.', '_', $item));
                 $params['openid.' . $item] = get_magic_quotes_gpc() ? stripslashes($val) : $val;
             }
 
@@ -111,7 +111,7 @@ class SteamLogin implements SteamLoginInterface {
 
             $result = file_get_contents(self::$openId, false, $context);
 
-            preg_match("#^http://steamcommunity.com/openid/id/([0-9]{17,25})#", $_GET['openid_claimed_id'], $matches);
+            preg_match("#^http://steamcommunity.com/openid/id/([0-9]{17,25})#", $this->request->input('openid_claimed_id'), $matches);
 
             $steamID64 = is_numeric($matches[1]) ? $matches[1] : 0;
 
